098c92221ce895f498fc02f195fcf358ddacfc48
Sanjeev Shrestha <sanjeev1.785401@cct.tu.edu.np>
2024-06-01 22:56:00 +0545

    first react project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// src/App.js
+import React from "react";
+// import "./App.css";
+// import MyButton from "./MyButton";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App</h1>
+        <MyButton />
+      </header>
+    </div>
+  );
+}
+
+export default App;
